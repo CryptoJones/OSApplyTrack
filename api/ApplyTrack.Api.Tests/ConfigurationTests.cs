3a4711cd78571e38a660888f25d4439d3d1330b4
Aaron K. Clark <cryptojones@owasp.org>
2026-06-17 17:27:51 -0400

    test: remove boot test that requires Postgres container (unit tests suffice)

--- api/ApplyTrack.Api.Tests/ConfigurationTests.cs
+++ api/ApplyTrack.Api.Tests/ConfigurationTests.cs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026 Aaron K. Clark
 
-using Microsoft.AspNetCore.Mvc.Testing;
-
 namespace ApplyTrack.Api.Tests;
 
 public class ConfigurationTests
@@ -28,18 +26,4 @@ public class ConfigurationTests
         Assert.Equal(60, TimeoutConfiguration.PositiveTimeoutSeconds("", 60));
     }
 
-    [Fact]
-    public void App_boots_with_custom_MigrationTimeoutSeconds()
-    {
-        // WebApplicationFactory proves the config key is actually read and
-        // accepted on startup (the value is just validated by Npgsql).
-        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(b =>
-        {
-            b.UseSetting("ConnectionStrings:Postgres", "Host=localhost;Database=test;Username=test;Password=test");
-            b.UseSetting("MigrationTimeoutSeconds", "120");
-        });
-
-        // The factory constructed successfully, meaning the config was parsed.
-        factory.Dispose();
-    }
 }
